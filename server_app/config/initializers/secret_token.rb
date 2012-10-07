39759410bf5061794679742c2292b0258f7e510e
Michael Landis <mlandis@Basil.(none)>
2012-10-06 20:53:26 -0400

    AUK-1: Created empty rails application

---
+++ server_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServerApp::Application.config.secret_token = 'fa4242cbe86320325ab7b81d52ed2d6b12e10beb23a1692f4b89dd075ec7998053cba26b43dd4e4d2cb1125f68fea529197e5014f582de66d2bfced747d11c3f'
